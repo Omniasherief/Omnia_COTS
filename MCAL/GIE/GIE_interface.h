9b57f631fafca8f15c7187461fb174d74de9ad83
Omnia <omniasheriefahmed@gmail.com>
2023-12-23 09:02:15 +0200

    Adding GIE Driver

---
+++ MCAL/GIE/GIE_interface.h
@@ -0,0 +1,15 @@
+/*
+ * GIE_interface.h
+ *
+ *  Created on: Dec 6, 2023
+ *      Author: omnia sherief
+ */
+
+#ifndef GIE_INTERFACE_H_
+#define GIE_INTERFACE_H_
+
+void GIE_voidEnableGlobal(void);
+void GIE_voidDisableGlobal (void);
+
+
+#endif /* GIE_INTERFACE_H_ */
